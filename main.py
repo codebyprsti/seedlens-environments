85926af4404da6d16a59f3a0ccab5130c85992ea
Madan Mohan Reddy M <madan.mohan@prsti.tech>
2025-07-16 16:59:20 +0530

    updated the main module

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ app.add_middleware(
 
 app.include_router(api_router, prefix="/api/v1")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8002, reload=False)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="127.0.0.1", port=8002, reload=False)
